9b8c97af9200de7e51057b2e8cdc68bdc14bc68f
any <any@user.tax>
2022-10-09 18:15:06 +0800

    ♨

--- translate.sh
+++ translate.sh
@@ -9,8 +9,8 @@ set -ex
 transalte(){
 bun run i18n -- -d $DIR/$1 en
 cd $DIR/$1
-cp zh.it zh.it.bak
-git checkout zh.it
+#cp zh.it zh.it.bak
+#git checkout zh.it
 opencc -i zh.it -c s2t -o zh-TW.it
 #sd -s ' ."' '"' $(fd -e json)
 #sd " \.$" "" $(fd -e it)
