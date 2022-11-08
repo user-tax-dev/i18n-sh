0ce815c654bbc8069a04e87b32c0d9c654c3a3fa
usrtax <i@user.tax>
2022-11-08 14:20:49 +0800

    ♨

--- translate.sh
+++ translate.sh
@@ -6,24 +6,24 @@ cd $DIR
 direnv allow
 
 set -ex
-transalte(){
-bun run i18n -- -d $DIR/$1 en
-cd $DIR/$1
-#cp zh.it zh.it.bak
-#git checkout zh.it
-opencc -i zh.it -c s2t -o zh-TW.it
-#sd -s ' ."' '"' $(fd -e json)
-#sd " \.$" "" $(fd -e it)
-cd $DIR
+transalte() {
+  bun run i18n -- -d $DIR/$1 en
+  cd $DIR/$1
+  #cp zh.it zh.it.bak
+  #git checkout zh.it
+  #opencc -i zh.it -c s2t -o zh-TW.it
+  #sd -s ' ."' '"' $(fd -e json)
+  #sd " \.$" "" $(fd -e it)
+  cd $DIR
 }
 
 transalte ..
 
 cd $DIR/..
 for lang in zh ja km lo th; do
-sd -s '？' '?' $lang.it
-sd -s ' ?' '?' $lang.it
-sd -s '?  ' '? ' $lang.it
+  sd -s '？' '?' $lang.it
+  sd -s ' ?' '?' $lang.it
+  sd -s '?  ' '? ' $lang.it
 done
 
 cd $DIR
